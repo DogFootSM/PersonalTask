c555107658c0b0814f7577951505e6589207aee5
Vivid <suook15@naver.com>
2024-07-22 16:19:59 +0900

    [Refactor] 주석 추가

--- 240722/Program.cs
+++ 240722/Program.cs
@@ -2,31 +2,50 @@
 {
     public class Character
     {
-        int level;
-        int hp;
-        int dmg;
-        float speed;
 
+        int level; //캐릭터 레벨
+        int hp;    //캐릭터 체력
+        int dmg;   //캐릭터 데미지
+        float speed;    //캐릭터 이동속도
+
+        /// <summary>
+        /// 캐릭터 전진 함수
+        /// </summary>
         public void Forward()
         {
         }
 
+        /// <summary>
+        /// 캐릭터 후진 함수
+        /// </summary>
         public void Backward()
         {
         }
 
+        /// <summary>
+        /// 캐릭터 좌회전 함수
+        /// </summary>
         public void TurnLeft()
         {
         }
 
+        /// <summary>
+        /// 캐릭터 우회전 함수
+        /// </summary>
         public void TurnRight()
         {
         }
 
+        /// <summary>
+        /// 캐릭터 공격 함수
+        /// </summary>
         public void Attack()
         {
         }
 
+        /// <summary>
+        /// 캐릭터 피격 함수
+        /// </summary>
         public void Defense()
         {
         }
