31d161a6aac928d02877127ea68af7f523c53570
Vivid <suook15@naver.com>
2024-07-22 15:44:53 +0900

    [Add]OOP Task
    2024.07.22 OOP 과제

---
+++ 240722/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _240722
+{
+    public class Character
+    {
+        int level;
+        int hp;
+        int dmg;
+        float speed;
+
+        public void Forward()
+        {
+        }
+
+        public void Backward()
+        {
+        }
+
+        public void TurnLeft()
+        {
+        }
+
+        public void TurnRight()
+        {
+        }
+
+        public void Attack()
+        {
+        }
+
+        public void Defense()
+        {
+        }
+
+    }
+
+    internal class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            Character character = new Character();
+             
+        }
+    }
+}
